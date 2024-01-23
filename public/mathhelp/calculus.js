b6efd4b8c84b556028854fb2adeba0b508d48475
GalacticNetwork <114846211+GalacticNetwork@users.noreply.github.com>
2024-01-23 03:41:11 +0000

    krunker

--- public/mathhelp/calculus.js
+++ public/mathhelp/calculus.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-  prefix: "/versat/",
+  prefix: "/versat-sw/",
   bare: "https://bare2.mysticmath.workers.dev/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
